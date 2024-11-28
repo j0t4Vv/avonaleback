e24ce64a1eadc95774da9728afc70d5e7d326275
gabrielmaia220 <159305213+gabrielmaia220@users.noreply.github.com>
2024-11-27 21:30:22 -0300

    Delete TaskManagerBackend directory

--- TaskManagerBackend/Dtos/UserDto.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskManagerBackend.Dtos
-{
-    public class UserDto
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
